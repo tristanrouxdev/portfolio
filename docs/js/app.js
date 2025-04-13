e3eff89b2622f15285a3dd822fe6bff9d0584b03
tristanroux <tristanroux69@gmail.com>
2025-04-13 20:43:14 +0200

    fin

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,3 +1,22 @@
 function toggleCarte(carte) {
     carte.classList.toggle('active');
   }
+
+
+function openLightbox(img) {
+  var lightbox = document.getElementById("lightbox");
+  var lightboxImg = document.getElementById("lightbox-img");
+  lightbox.style.display = "block";
+  lightboxImg.src = img.src;
+}
+
+document.querySelector(".close").onclick = function () {
+  document.getElementById("lightbox").style.display = "none";
+};
+
+// Fermer en cliquant à l’extérieur de l’image
+document.getElementById("lightbox").onclick = function (event) {
+  if (event.target === this) {
+    this.style.display = "none";
+  }
+};
